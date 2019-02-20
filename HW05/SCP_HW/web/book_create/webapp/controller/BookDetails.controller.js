71479a79d680578d5af7cffa697151afe0c84e4e
Aliona Sauchuk <aliona.sauchuk@LeverX.local>
2019-02-20 14:54:28 +0300

    in process

--- HW05/SCP_HW/web/book_create/webapp/controller/BookDetails.controller.js
+++ HW05/SCP_HW/web/book_create/webapp/controller/BookDetails.controller.js
@@ -1,14 +1,76 @@
 sap.ui.define([
 	"book_create/controller/BaseController",
-	"sap/ui/core/routing/History"
-],function (BaseController, History) {
+    "sap/ui/core/routing/History",
+    'sap/ui/core/Fragment',
+],function (BaseController, History,  Fragment) {
 	"use strict";
 	return BaseController.extend("book_create.controller.BookDetails", {
 		onInit: function(){
 			var oRouter = this.getRouter();
 			oRouter.getRoute("details").attachPatternMatched(this._onObjectMatched, this);
+  
+			this.getView().getModel().attachRequestCompleted(function() {
+				this.byId('editButton').setEnabled(true);
+			}.bind(this));
+
+
+        },
+
+		_showFormFragment : function (sFragmentName) {
+			var oPage = this.byId("page");
+
+			oPage.removeAllContent();
+			oPage.insertContent(this._getFormFragment(sFragmentName));
+        },
+
+        _getFormFragment: function (sFragmentName) {
+			var oFormFragment = this._formFragments[sFragmentName];
+
+			if (oFormFragment) {
+				return oFormFragment;
+			}
+
+			oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "book_create." + sFragmentName);
+
+			this._formFragments[sFragmentName] = oFormFragment;
+			return this._formFragments[sFragmentName];
+		},
+		
+        
+		onEdit: function() {
+			this.byId("editButton").setVisible(false);
+			this.byId("saveButton").setVisible(true);
+            this.byId("cancelButton").setVisible(true);
+            this.getView().setEditable(true);
 		},
 
+		onSave: function() {
+			this.byId("saveButton").setVisible(false);
+			this.byId("cancelButton").setVisible(false);
+			this.byId("editButton").setVisible(true);
+			this.rebindForm(this.oReadOnlyTemplate, "Navigation");
+		},
+
+		onCancel: function() {
+			this.byId("cancelButton").setVisible(false);
+			this.byId("saveButton").setVisible(false);
+			this.byId("editButton").setVisible(true);
+			this.rebindForm(this.oReadOnlyTemplate, "Navigation");
+		},
+
+		onOrder: function() {
+			MessageToast.show("Order button pressed");
+		},
+
+		onExit: function() {
+			this.aBookCollection = [];
+			this.oEdiFormTemplate.destroy();
+		},
+
+		onPaste: function(oEvent) {
+			var aData = oEvent.getParameter("data");
+			sap.m.MessageToast.show("Pasted Data: " + aData);
+		},
 
 		_onObjectMatched: function (oEvent) {
 			this.getView().bindElement({
@@ -22,7 +84,6 @@ sap.ui.define([
 		onNavBack: function () {
 
 			var oHistory, sPreviousHash;
-	  
 			oHistory = History.getInstance();
 			sPreviousHash = oHistory.getPreviousHash();
 	  
--- HW05/SCP_HW/web/book_create/webapp/controller/Home.controller.js
+++ HW05/SCP_HW/web/book_create/webapp/controller/Home.controller.js
@@ -11,27 +11,13 @@ sap.ui.define([
 		/**
 		 *  Hook for initializing the BaseController
 		 */
-		onInit : function () {
-			/*
-			this.oTable = this.byId("bookTable");
-			this.oReadOnlyTemplate = this.byId("bookTable").removeItem(0);
-			this.rebindTable(this.oReadOnlyTemplate, "Navigation");
-			this.oEditableTemplate = new sap.m.ColumnListItem({
-				cells: [
-					new sap.m.Input({
-						value: "{books>caption}"
-					})
-				]
-			});			
-			*/
-			
+		onInit : function () {	
 		},
 
 		onItemSelected: function(oEvent) {
 
 			var oSelectedItem = oEvent.getSource();
 			var context = encodeURIComponent(oSelectedItem.getBindingContext('books').getPath());
-			console.log(context);
 			this.getRouter().navTo("details",  {bookID: context});
             
 		},
@@ -50,52 +36,7 @@ sap.ui.define([
 			var list = this.byId("bookList");
 			var binding = list.getBinding("items");
 			binding.filter(aFilters, "Application");
-		},
-
-/*
-		rebindTable: function(oTemplate, sKeyboardMode) {		
-			this.oTable.bindItems({
-				path: "books>/Books",
-				template: oTemplate,
-				key: "books>caption"
-			}).setKeyboardMode(sKeyboardMode);
-		},
-
-		onEdit: function() {
-			this.byId("editButton").setVisible(false);
-			this.byId("saveButton").setVisible(true);
-			this.byId("cancelButton").setVisible(true);
-			this.rebindTable(this.oEditableTemplate, "Edit");
-		},
-
-		onSave: function() {
-			this.byId("saveButton").setVisible(false);
-			this.byId("cancelButton").setVisible(false);
-			this.byId("editButton").setVisible(true);
-			this.rebindTable(this.oReadOnlyTemplate, "Navigation");
-		},
-
-		onCancel: function() {
-			this.byId("cancelButton").setVisible(false);
-			this.byId("saveButton").setVisible(false);
-			this.byId("editButton").setVisible(true);
-			this.rebindTable(this.oReadOnlyTemplate, "Navigation");
-		},
-
-		onOrder: function() {
-			MessageToast.show("Order button pressed");
-		},
-
-		onExit: function() {
-			this.aBookCollection = [];
-			this.oEditableTemplate.destroy();
-		},
-
-		onPaste: function(oEvent) {
-			var aData = oEvent.getParameter("data");
-			sap.m.MessageToast.show("Pasted Data: " + aData);
 		}
-		*/
 	});
 
 	return TableController;
